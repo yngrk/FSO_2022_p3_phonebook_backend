f0c59972c6606f5ca6e82a434d75f5659dea5eb5
yngrk <yngrk.kim@gmail.com>
2023-02-23 12:49:22 +0100

    removed prettier and set eslint as formatter

--- mongo.js
+++ mongo.js
@@ -1,14 +1,14 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 if (process.argv.length < 3) {
-  console.log("err: require password");
+  console.log('err: require password');
   process.exit(1);
 }
 
 const password = process.argv[2];
 const url = `mongodb+srv://yngrk:${password}@cluster0.aeqrq6y.mongodb.net/phonebook?retryWrites=true&w=majority`;
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false);
 
 mongoose.connect(url);
 
@@ -17,12 +17,12 @@ const personSchema = new mongoose.Schema({
   number: String,
 });
 
-const Person = mongoose.model("Person", personSchema);
+const Person = mongoose.model('Person', personSchema);
 
 // GET ALL ENTRIES
 if (process.argv.length < 4) {
   Person.find({}).then((result) => {
-    console.log("phonebook:");
+    console.log('phonebook:');
     result.forEach((person) => {
       console.log(person.name, person.number);
     });
@@ -33,12 +33,12 @@ if (process.argv.length < 4) {
   // ADD NEW PERSON
   const person = new Person({
     name: process.argv[3],
-    number: process.argv[4] || "",
+    number: process.argv[4] || '',
   });
 
-  person.save().then((result) => {
+  person.save().then(() => {
     console.log(
-      `added ${process.argv[3]} number ${process.argv[4] || "N/A"} to phonebook`
+      `added ${process.argv[3]} number ${process.argv[4] || 'N/A'} to phonebook`,
     );
     mongoose.connection.close();
   });
